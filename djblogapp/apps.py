6009d41f154d0507072e6fa1b092b549f042561b
barnez299 <barnez29@gmail.com>
2021-03-03 16:12:36 +0200

    static folder and templates added

--- djblogapp/apps.py
+++ djblogapp/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class DjblogappConfig(AppConfig):
+class djblogappConfig(AppConfig):
     name = 'djblogapp'
--- djblogapp/views.py
+++ djblogapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+
+
+
+
 
 # Create your views here.
+
+def home(request):
+    posts = Post.objects.all()
+
+    return render(request, 'djblogapp/index.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'djblogapp/about.html')
